93a959be16964b9ead661477e6520e2b6cb342e9
marcelomonteiro1 <80854714+marcelomonteiro1@users.noreply.github.com>
2021-08-16 14:55:56 -0300

    Criando Front

--- web/src/App.js
+++ web/src/App.js
@@ -1,16 +1,37 @@
 import React from 'react';
-import PromtionCarros from './components/Promotion/Carros/carros';
-import './App.css';
+import PromotionCarros from './components/Promotion/Carros/carros';
 
 const App = () => {
-  
-  return(
-      <div className="App">
-        <PromtionCarros test="cades"/>
-      </div>
+  const promotion = {
+    "id": 1,
+      "nome": "Honda Civc",
+      "km_por_galao": 18,
+      "cilindros": 8,
+      "cavalor_de_forca": 500,
+      "peso": 3504,
+      "aceleracao": 12,
+      "ano": "2009-01-01",
+      "origem": "USA",
+      "marcas": [
+        {
+          "id": 1,
+          "Marca": "Honda",
+        }
+      ],
+  };
+
+
+  return (
+    <div
+      className="App"
+      style={{
+        maxWidth: 800,
+        margin: '30px auto',
+      }}
+    >
+      <PromotionCarros promotion={promotion} />
+    </div>
   );
-
-
 }
 
 export default App;
--- web/src/components/Promotion/Carros/carros.js
+++ web/src/components/Promotion/Carros/carros.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import './carros.css';
 
-const PromtionCarros = () => (
-    
-    <div> HI</div>
-
+const PromotionCarros = ({ promotion }) => (
+  <div className="promotion-carros">
+    <img src={promotion.imageUrl} className="promotion-carros__image" />
+    <div className="promotion-carros__info">
+      <h1 className="promotion-carros__title">{promotion.nome}</h1>
+      <span className="promotion-carros__price">CV{promotion.cavalor_de_forca}</span>
+    </div>
+  </div>
 );
-export default PromtionCarros;
+
+export default PromotionCarros;
